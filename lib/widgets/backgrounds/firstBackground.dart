4ed0596573cebd372aa935783edc42a62f237c01
Imperial-lord <absatyaprakash01@gmail.com>
2021-03-02 12:21:31 +0530

    :tada: feat: added welcome screen

---
+++ lib/widgets/backgrounds/firstBackground.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:health_bag/globals/myColors.dart';
+
+class FirstBackground extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Material(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: MyColors.blue,
+            child: CustomPaint(
+              painter: OpenPainter(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class OpenPainter extends CustomPainter{
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = MyColors.blueLighter
+      ..style = PaintingStyle.fill;
+    canvas.drawCircle(Offset(size.width/2, size.height/1.3), size.width/2, paint1);
+
+    var paint2 = Paint()
+      ..color = MyColors.red
+      ..style = PaintingStyle.fill;
+    canvas.drawCircle(Offset(size.width, size.height), 100, paint2);
+
+    var paint3 = Paint()
+      ..color = MyColors.white
+      ..style = PaintingStyle.fill;
+
+    var path = Path();
+    path.moveTo(0, size.height * 0.6);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.7,
+        size.width * 0.5, size.height * 0.6);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.5,
+        size.width * 1.0, size.height * 0.6);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint3);
+  }
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
